613a1e954b12ddf368557cb55024be4e1ca0d003
mateusz <matek1600@o2.pl>
2022-06-20 11:16:05 +0200

    first comit

--- dental/settings.py
+++ dental/settings.py
@@ -128,12 +128,13 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.mail.yahoo.com'
-EMAIL_PORT= 465
+EMAIL_PORT= 587
 EMAIL_HOST_USER = 'mateuszelo1@yahoo.com'
 EMAIL_HOST_PASSWORD ='myszkaasia1'
-#EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
+#EMAIL_USE_SSL = True
 
 
 '''
